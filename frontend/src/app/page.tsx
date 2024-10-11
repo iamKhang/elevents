a72ec48dae72d221eec0f032893fb5dab50736ec
iamKhang <iamhoangkhang@icloud.com>
2024-10-11 20:39:36 +0700

    Xử lý lỗi thư viện

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div>
       <Header />
-      {/* <Carousel /> */}
+      <Carousel />
       <main className="p-4 mt-20">
         <h1 className="text-2xl font-bold">Welcome to the Elevents Shop!</h1>
         <p className="text-gray-700">Explore our collection of unique and stylish apparel.aawf</p>
